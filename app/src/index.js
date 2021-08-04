8a85eca39a7d0d658176a5dde2125f05829f0c97
BM-ska <barbaramoczulska1a@gmail.com>
2021-08-04 18:09:21 +0200

    Add basic form

--- app/src/index.js
+++ app/src/index.js
@@ -10,5 +10,48 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+class OwnerForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: "undeclared",
+            cacheName: "undeclared",
+            cacheNumber: "undeclared",
+            ifHint: false,
+            whenHint: 0
+        };
 
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
 
+    handleInputChange(event) {
+
+    }
+
+    render() {
+        return (
+            <form>
+                <label>Do you want a hint?: &nbsp; </label>
+                <input
+                    name="ifHint"
+                    type="checkbox"
+                    checked={this.state.ifHint}
+                    onChange={this.handleInputChange} />
+
+                <br />
+                <label>After how many wrong answers?: &nbsp; </label>
+                <input
+                    name="whenHint"
+                    type="number"
+                    value={this.state.whenHint}
+                    onChange={this.handleInputChange} />
+
+            </form>
+        );
+    }
+}
+
+ReactDOM.render(
+    <OwnerForm />,
+    document.getElementById('root')
+);
